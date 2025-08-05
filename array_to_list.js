c21eaa6366113e392f18de0a8cd00b14d2e5be8d
dalim-kazi <mddalim862483@gmail.com>
2025-08-05 23:53:53 +0600

    merge list

--- array_to_list.js
+++ array_to_list.js
@@ -9,11 +9,42 @@ function arrayToList(arr) {
 
   for (let val of arr) {
     current.next = new ListNode(val);
-    current == current.next;
+    current = current.next;
   }
 
-  return current.next;
+  return dummy.next;
 }
 
-const list1 = arrayToList([1, 3, 5]);
-console.log(list1);
+function listToArray(head) {
+  let result = [];
+  while (head) {
+    result.push(head.val);
+    head = head.next;
+  }
+  return result;
+}
+
+function mergeTwoLists(list1, list2) {
+  const dummy = new ListNode(-1);
+  let current = dummy;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+
+  current.next = list1 || list2;
+
+  return dummy.next;
+}
+
+const list1 = arrayToList([1, 2, 4]);
+const list2 = arrayToList([1, 3, 4]);
+const merged = mergeTwoLists(list1, list2);
+console.log(listToArray(merged));
