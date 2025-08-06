7a99631a2c81bda1cf768ddbf9e072f7e0b5542c
dalim-kazi <mddalim862483@gmail.com>
2025-08-06 23:25:14 +0600

    update

--- to_sum.js
+++ to_sum.js
@@ -1,15 +1,37 @@
 function toSum(arr, target) {
+  console.time("time first");
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       const element2 = arr[j];
       const element1 = arr[i];
       if (element1 + element2 === target) {
+        console.timeEnd("time first");
         return [i, j];
       }
     }
   }
 }
 
-const result = toSum([12, 34, 6, 7, 2, 8], 9);
+function toSumPointer(arr, target) {
+  console.time("sce");
+  const sortArr = arr.sort((a, b) => a - b);
+  let first = 0;
+  let last = sortArr.length - 1;
+  while (first < last) {
+    let sum = sortArr[first] + sortArr[last];
+    if (sum === target) {
+      console.timeEnd("sce");
+      return [first, last];
+    } else if (sum > target) {
+      last--;
+    } else {
+      first++;
+    }
+  }
+  console.log(first, last);
+}
 
-console.log(result);
+const result = toSum([12, 34, 6, 7, 2, 8], 9);
+const result2 = toSumPointer([12, 34, 6, 7, 2, 8], 9);
+
+console.log(result2);
