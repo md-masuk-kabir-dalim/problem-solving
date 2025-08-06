11519ea78d0b72b65878572e0f0197ca3b337622
dalim-kazi <mddalim862483@gmail.com>
2025-08-06 21:05:46 +0600

    composite funcation

--- this_keyword.js
+++ this_keyword.js
@@ -7,4 +7,18 @@ const parson = {
   },
 };
 
+const x = 10;
+function f(x) {
+  return x + 3;
+}
+function g(x) {
+  return x * 2;
+}
+
+function composite(params) {
+  return f(g(params));
+}
+
+const result = composite(2);
+console.log(result);
 console.log(parson.fullName());
