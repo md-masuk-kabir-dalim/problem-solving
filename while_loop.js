c5c722c422dcf13d9999531ae154416166437b97
dalim-kazi <mddalim862483@gmail.com>
2025-08-10 20:08:14 +0600

    while loop

---
+++ while_loop.js
@@ -0,0 +1,12 @@
+let count = 0;
+while (count <= 5) {
+  console.log(count);
+  count++;
+}
+
+let arr = [10, 20, 30, 40];
+let i = 0;
+while (i < arr.length - 1) {
+  console.log(arr[i]);
+  i++;
+}
